5cbd7b6ae496aa3f4b3d255fd888bba1bde8d8a5
tk <tk@renard.ninja>
2021-11-15 13:36:56 +0000

    configures base URL

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,6 +3,7 @@ const { path } = require("@vuepress/utils");
 module.exports = {
   title: "DoctoScan",
   theme: path.resolve(__dirname, "./theme"),
+  base: "/ocrxtract-site/",
   markdown: {
     anchor: false,
   },
